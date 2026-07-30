3b4cb1716fb0761de854f35c44748ad45004312e
TeaRediT <vlad.bobro.02@gmail.com>
2026-07-30 16:23:01 +0300

    add register and login func

---
+++ lib/api.ts
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+interface RegCreds {
+  name: string;
+  email: string;
+  password: string;
+}
+
+interface LoginCreds {
+  email: string;
+  password: string;
+}
+
+interface AuthRes {
+  email: string;
+  name: string;
+  token: string;
+  refreshToken: string;
+}
+
+export const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+});
+
+export const setAuthHeader = (token: string) => {
+  api.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
+
+export const clearAuthHeader = () => {
+  api.defaults.headers.common.Authorization = "";
+};
+
+export const registerUser = async (creds: RegCreds): Promise<AuthRes> => {
+  const { data } = await api.post<AuthRes>("/users/signup", creds);
+  return data;
+};
+
+export const loginUser = async (creds: LoginCreds): Promise<AuthRes> => {
+  const { data } = await api.post<AuthRes>("/users/signin", creds);
+  return data;
+};
