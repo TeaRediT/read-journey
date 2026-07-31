cf33f9f19052967a91aede786f137a0dbc6f4d46
TeaRediT <vlad.bobro.02@gmail.com>
2026-07-31 20:58:15 +0300

    add quote component

---
+++ components/Recommended/Quote.tsx
@@ -0,0 +1,11 @@
+export const Quote = () => {
+  return (
+    <div className="hidden ds:flex items-center justify-between gap-3.5 rounded-xl bg-surface-light px-5 py-3.75">
+      <p className="text-[40px] leading-none">📚</p>
+      <p className="text-secondary">
+        &quot;Books are <span className="text-primary">windows </span> to the
+        world, and reading is a journey into the unknown.&quot;
+      </p>
+    </div>
+  );
+};
