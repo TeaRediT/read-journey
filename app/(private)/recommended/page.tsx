2be187171aecf09fb9251b2c4c6a00cd38fc605e
TeaRediT <vlad.bobro.02@gmail.com>
2026-07-31 21:00:03 +0300

    add recommended page

--- app/(private)/recommended/page.tsx
+++ app/(private)/recommended/page.tsx
@@ -1,5 +1,15 @@
-const Recommended = () => {
-  return <main></main>;
+import Container from "@/components/common/Container/Container";
+import RecommendedClient from "./recommended.client";
+
+const Recommended = async () => {
+  return (
+    <main>
+      <Container>
+        <h1 className="sr-only">Recommended Books Page</h1>
+        <RecommendedClient />
+      </Container>
+    </main>
+  );
 };
 
 export default Recommended;
