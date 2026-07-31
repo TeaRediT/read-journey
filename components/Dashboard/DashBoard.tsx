db4d0033650de9826f528f0802fe757ed6599289
TeaRediT <vlad.bobro.02@gmail.com>
2026-07-31 20:58:06 +0300

    add dashboard component

---
+++ components/Dashboard/DashBoard.tsx
@@ -0,0 +1,11 @@
+interface DashboardProps {
+  children: React.ReactNode;
+}
+
+export const Dashboard = ({ children }: DashboardProps) => {
+  return (
+    <section className="flex flex-col gap-5 rounded-[30px] bg-surface p-5 my-2.5 md:flex-row md:justify-between md:gap-0 md:p-8 md:my-4 ds:flex-col ds:justify-normal ds:gap-5 ds:p-5 ds:pt-10 ds:w-88.25">
+      {children}
+    </section>
+  );
+};
