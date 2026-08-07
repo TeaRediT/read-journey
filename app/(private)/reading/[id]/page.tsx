74df719fff8bdf09707f401fe6882508d4416948
TeaRediT <vlad.bobro.02@gmail.com>
2026-08-07 21:41:16 +0300

    add / page

---
+++ app/(private)/reading/[id]/page.tsx
@@ -0,0 +1,14 @@
+import Container from "@/components/common/Container/Container";
+import ReadingClient from "./Reading.client";
+
+const Reading = () => {
+  return (
+    <main>
+      <Container>
+        <ReadingClient />
+      </Container>
+    </main>
+  );
+};
+
+export default Reading;
